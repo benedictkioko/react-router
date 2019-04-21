6b36fa08eaf07cc5da6e3b3dc1f688ff98841705
Benedict Kioko <kiokobenedict@gmail.com>
2019-04-22 00:52:53 +0300

    Added components and routing

---
+++ src/components/Error.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const Error = () => {
+
+    return (
+        <div>
+            <p>Seems you are lost. Damn! thats 404!</p>
+        </div>
+    );
+
+}
+
+export default Error;
